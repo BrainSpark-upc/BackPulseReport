b8d17501a3600c21c50ec407773bb60097a3c7a4
Marcelo Mansilla <c3sv.19@gmail.com>
2026-05-27 08:23:49 -0500

    chore: initial project setup

---
+++ src/test/java/com/brainspark/pulsereport/platform/BackendPulsereportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brainspark.pulsereport.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPulsereportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
