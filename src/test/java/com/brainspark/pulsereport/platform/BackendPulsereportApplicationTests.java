728b57406fa880b31b097b3b8d819195d7a45556
Marcelo Mansilla <c3sv.19@gmail.com>
2026-06-02 10:01:57 -0500

    fix(shared): align shared bounded context configuration

--- src/test/java/com/brainspark/pulsereport/platform/BackendPulsereportApplicationTests.java
+++ src/test/java/com/brainspark/pulsereport/platform/BackendPulsereportApplicationTests.java
@@ -3,11 +3,14 @@ package com.brainspark.pulsereport.platform;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+@SpringBootTest(properties = {
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+})
 class BackendPulsereportApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
+
