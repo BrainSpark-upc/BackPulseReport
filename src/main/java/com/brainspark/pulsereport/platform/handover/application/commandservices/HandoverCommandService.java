25469f1d7874fecef5a7926f5e3823adad38547b
Johancuba <168486887+Johancuba@users.noreply.github.com>
2026-06-08 09:49:15 -0500

    feat(handover): create files to endpoint's post

---
+++ src/main/java/com/brainspark/pulsereport/platform/handover/application/commandservices/HandoverCommandService.java
@@ -0,0 +1,4 @@
+package com.brainspark.pulsereport.platform.handover.application.commandservices;
+
+public class HandoverCommandService {
+}
---
+++ src/main/java/com/brainspark/pulsereport/platform/handover/domain/model/commands/CreateHandoverCommand.java
@@ -0,0 +1,5 @@
+package com.brainspark.pulsereport.platform.handover.domain.model.commands;
+
+public class CreateHandoverCommand {
+
+}
