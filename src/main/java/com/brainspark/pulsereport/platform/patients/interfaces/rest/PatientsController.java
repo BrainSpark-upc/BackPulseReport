33b1f676039a4c89074e725e9f77d0308cd7f636
AlexanderAliaga19 <u202417693@upc.edu.pe>
2026-06-10 09:07:10 -0500

    feat(patients): expose create patient endpoint

---
+++ src/main/java/com/brainspark/pulsereport/platform/patients/interfaces/rest/PatientsController.java
@@ -0,0 +1,38 @@
+package com.brainspark.pulsereport.platform.patients.interfaces.rest;
+
+import com.brainspark.pulsereport.platform.patients.application.commandservices.PatientCommandService;
+import com.brainspark.pulsereport.platform.patients.interfaces.rest.resources.CreatePatientResource;
+import com.brainspark.pulsereport.platform.patients.interfaces.rest.transform.CreatePatientCommandFromResourceAssembler;
+import com.brainspark.pulsereport.platform.patients.interfaces.rest.transform.PatientResourceFromEntityAssembler;
+import com.brainspark.pulsereport.platform.shared.interfaces.rest.transform.ResponseEntityAssembler;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping(value = "/api/v1/patients", produces = APPLICATION_JSON_VALUE)
+@Tag(name = "Patients", description = "Patient management endpoints")
+public class PatientsController {
+
+    private final PatientCommandService patientCommandService;
+
+    public PatientsController(PatientCommandService patientCommandService) {
+        this.patientCommandService = patientCommandService;
+    }
+
+    @PostMapping(consumes = APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> createPatient(@RequestBody @Valid CreatePatientResource resource) {
+        var command = CreatePatientCommandFromResourceAssembler.toCommandFromResource(resource);
+        var result = patientCommandService.handle(command);
+
+        return ResponseEntityAssembler.toResponseEntityFromResult(
+                result,
+                PatientResourceFromEntityAssembler::toResourceFromEntity,
+                HttpStatus.CREATED
+        );
+    }
+}
